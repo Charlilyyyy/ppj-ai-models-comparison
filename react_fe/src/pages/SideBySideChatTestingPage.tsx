c8cc24aad6fa971d04a36cf848c2987f05316867
taufikcharlie <mohamadtaufik099@gmail.com>
2025-08-11 21:31:41 +0800

    sidebysidechattestingpage fe

--- react_fe/src/pages/SideBySideChatTestingPage.tsx
+++ react_fe/src/pages/SideBySideChatTestingPage.tsx
@@ -6,6 +6,19 @@ import {
     navigationMenuTriggerStyle,
   } from "@/components/ui/navigation-menu"
 import { Link } from 'react-router-dom'
+import {
+    Card,
+    // CardAction,
+    CardContent,
+    // CardDescription,
+    // CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+import { Button } from "@/components/ui/button"
+import { aiModels } from "@/data/ai-models"
+import { Input } from "@/components/ui/input"
+import { Badge } from "@/components/ui/badge"
 
 export default function SideBySideChatTestingPage(){
     return(
@@ -19,7 +32,48 @@ export default function SideBySideChatTestingPage(){
                     </NavigationMenuItem>
                 </NavigationMenuList>
             </NavigationMenu>
-            <h1>SideBySideChatTestingPage</h1>
+            <div>
+                <div className="flex justify-center">
+                    <h1 className="font-bold underline">Side By Side Chat Testing</h1>
+                </div>
+                <div className="flex justify-center">
+                    <Card className="w-2xl mx-5 my-10">
+                        <CardHeader>
+                            <CardTitle>Chatbot</CardTitle>
+                        </CardHeader>
+                        <CardContent>
+                            <div className="chat border-solid border-2 rounded-md border-slate-700">
+                                <div className="mx-5 my-5">
+                                    <p className="text-gray-400">Chatbot chat history</p>
+                                </div>
+                            </div>
+                            <div className="flex mt-2 gap-2">
+                                <Input placeholder="Type question ..."/>
+                            <Button className="">Send</Button>
+                            </div>
+                        </CardContent>
+                    </Card>
+                </div>
+                <h2 className="text-center mb-4 font-bold underline text-2xl">Results</h2>
+                <div className="grid grid-cols-3 gap-2">
+                    {aiModels.map((model) => {
+                        return(
+                            <Card className="w-md mx-5 my-2">
+                                <CardHeader>
+                                    <CardTitle>
+                                        <Badge variant="secondary">
+                                            {model.name} Chatbot
+                                        </Badge>
+                                    </CardTitle>
+                                </CardHeader>
+                                <CardContent>
+                                
+                                </CardContent>
+                            </Card>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
